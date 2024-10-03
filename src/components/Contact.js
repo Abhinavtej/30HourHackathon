d588f09deb5255c6c91d50a75658624edf221117
Abhinavtej Reddy <87389936+Abhinavtej@users.noreply.github.com>
2024-10-03 17:09:04 +0530

    contact updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,7 +35,6 @@ function Contact() {
                 <p>Email: nispmruh@mallareddyuniversity.ac.in</p>
                 <p>Location:</p>
                 <iframe title="Malla Reddy University Location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3803.8890264176202!2d78.44627937516894!3d17.560469433356616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb8ff8652b6823%3A0x981713dbb4b708c3!2sMalla%20Reddy%20University!5e0!3m2!1sen!2sin!4v1727342094485!5m2!1sen!2sin" width="300" height="200" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                <br /><br /><span>{result}</span>
             </div>
             <div className="contact-form">
                 <form onSubmit={onSubmit}>
@@ -45,8 +44,9 @@ function Contact() {
                     <input type="email" name="email" required /> <br />
                     <label htmlFor="message">Message:</label> <br />
                     <textarea name="message" required></textarea> <br />
-                    <button class="btn btn-primary">Send</button>
+                    <button>Send</button>
                 </form>
+                <span>{result}</span>
             </div>
         </div>
     </div>
