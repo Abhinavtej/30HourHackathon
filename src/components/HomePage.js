b4f76b2cfa067eb931be57f35e24be87dafeb48f
Abhinavtej Reddy <87389936+Abhinavtej@users.noreply.github.com>
2024-11-24 00:33:00 +0530

    added spinner

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import './css/HomePage.css'
-import Title from '../assets/title.png'
+import React, { useState } from 'react';
+import './css/HomePage.css';
+import Title from '../assets/title.png';
 
 function HomePage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
-    <div className='App-HomePage'>
-      <img src={Title} width={1000} alt="Title" />
+    <div className="App-HomePage">
+      {isLoading && (
+        <div className="spinner">
+          <div className="loader"></div>
+        </div>
+      )}
+      <img
+        src={Title}
+        width={1000}
+        alt="Title"
+        onLoad={handleImageLoad}
+        className={isLoading ? 'hidden' : ''}
+      />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
